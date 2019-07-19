229a5b2a4115ea4493a364fcbd37729ee5bcc883
Mikaël Barbero <mikael.barbero@gmail.com>
2019-07-20 00:39:42 +0200

    Revamped notifications

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -1,35 +1,59 @@
 #!/usr/bin/env groovy
 
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
+
 /**
  * Send notifications based on build status string
  */
-def call(String buildStatus = 'STARTED') {
+def call(RunWrapper currentBuild) {
   // build status of null means successful
-  buildStatus =  buildStatus ?: 'SUCCESS'
+  buildStatus =  currentBuild.result ?: 'SUCCESS'
+  previousBuildStatus =  currentBuild.previousBuild?.result ?: 'SUCCESS'
 
   // Default values
-  def color = 'RED'
   def colorCode = '#A20E10'
-  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def summary = "${subject} (${env.BUILD_URL})"
-  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+  def subject = ''
 
-  // Override default values based on build status
-  if (buildStatus == 'STARTED') {
-    color = 'YELLOW'
-    colorCode = '#F1C750'
-  } else if (buildStatus == 'SUCCESS' || buildStatus == 'FIXED') {
-    color = 'GREEN'
+  if (buildStatus == 'ABORTED') {
+    colorCode = '#A2A2A2'
+    if (previousBuildStatus != 'SUCCESS') {
+      subject = 'Aborted'
+    }
+  } else if (buildStatus == 'FAILURE') {
+    colorCode = '#A20E10'
+    if (previousBuildStatus == 'FAILURE') {
+      subject = 'Still failing'
+    } else {
+      subject = 'Broken'
+    }
+  } else if (buildStatus == 'UNSTABLE') {
+    colorCode = '#E5AD00'
+    if (previousBuildStatus == 'UNSTABLE') {
+      subject = 'Still unstable'
+    } else {
+      subject = "Unstable"
+    }
+  } else { // buildStatus == "SUCCESS"
     colorCode = '#34B787'
+    if (previousBuildStatus != 'SUCCESS') {
+      subject = "Back to normal"
+    }
+  }
+  
+  if (subject != '') {
+    slackSend (color: colorCode, message: "${env.JOB_NAME} ${env.BUILD_NUMBER} - ${subject} (<a href=\"${env.BUILD_URL}/console\">logs</a>)")
+
+    emailext (
+      subject: "${subject}: ${env.JOB_NAME} ${env.BUILD_NUMBER}",
+      body: """
+        <p><a href='${env.BUILD_URL}'>See console output at ${env.BUILD_URL}</a></p>
+        <p>----</p>
+        <pre>${currentBuild.rawBuild.log(128).join('<br/>')}</pre>
+      """,
+      recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
+    )
   }
 
   // Send notifications
-  slackSend (color: colorCode, message: summary)
-
-  emailext (
-      subject: subject,
-      body: details,
-      recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']]
-    )
+  
 }
